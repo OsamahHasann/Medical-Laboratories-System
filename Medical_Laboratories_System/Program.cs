262ee27bccdc5c6382ecd1185b48d97db4bd9649
ENG2Osama <Eng.osama.a.f@gmail.com>
2025-12-07 18:33:28 +0300

    add test to database

--- Medical_Laboratories_System/Program.cs
+++ Medical_Laboratories_System/Program.cs
@@ -14,6 +14,8 @@ namespace Medical_Laboratories_System
         [STAThread]
         static void Main()
         {
+           
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login_in());
